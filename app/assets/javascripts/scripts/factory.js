8ad21df405728764d056a156e9f7334523e4f9d2
neophytes08 <allan.alzula@gmail.com>
2015-07-04 10:40:48 +0800

    edited factory url /

--- app/assets/javascripts/scripts/factory.js
+++ app/assets/javascripts/scripts/factory.js
@@ -2,7 +2,7 @@ app
   .factory( "CarServer" , [
     "$http",
     function factory ( $http ) {
-      var host = "https://glacial-everglades-2781.herokuapp.com/";
+      var host = "https://glacial-everglades-2781.herokuapp.com";
 
       this.request  = function request ( method , path , callback , data ) {
         $http[ method ]( host + path , ( data || { } ) )

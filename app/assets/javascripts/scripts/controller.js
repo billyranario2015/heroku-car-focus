79d66784ca08e8a6c92b78f018ec5310a6997191
Billy Joel Ranario <billy.ranario@must.edu.ph>
2015-07-05 15:48:11 +0800

    dasd

--- app/assets/javascripts/scripts/controller.js
+++ app/assets/javascripts/scripts/controller.js
@@ -121,21 +121,21 @@ app
        $( 'body' ).delegate( '.edit-selection' , 'click' , function(){
         var ID = $( this ).attr( 'id' );
          $.each( $scope.addOnStock , function(i){
+            $timeout(function(){
+              $scope.dataEdit.price = $scope.addOnStock[i].price;
+              $scope.dataEdit.product_name = $scope.addOnStock[i].product_name;
+              $scope.dataEdit.product_details = $scope.addOnStock[i].product_details;
+              $scope.dataEdit.product_type = $scope.addOnStock[i].product_type;
+              $scope.dataEdit.quantity = $scope.addOnStock[i].quantity;
+            },10);
 
-              console.log( $scope.addOnStock[i]);
-              console.log( $scope.addOnStock[i].cartID +' == '+ ID );
-
-              $timeout(function(){
-                $scope.dataEdit.price = $scope.addOnStock[i].price;
-                $scope.dataEdit.product_name = $scope.addOnStock[i].product_name;
-                $scope.dataEdit.product_details = $scope.addOnStock[i].product_details;
-                $scope.dataEdit.product_type = $scope.addOnStock[i].product_type;
-                $scope.dataEdit.quantity = $scope.addOnStock[i].quantity;
-              },10)
-              // if($scope.addOnStock[i].cartID == ID) {
-              // }
           });
        } );
+
+       // Make all Changes for ordered items 
+       $scope.updateOrderedProduct = function updateOrderedProduct() {
+          console.log( $scope.dataEdit );
+       } 
 
        function getTotal( orderValue , operation ) {
         var total = 0;
